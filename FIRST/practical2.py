b64cd8ffd52f0ac1e92b4000738e6883a9e3220f
dhruv kumar yadav <444dhruv@gmail.com>
2024-03-05 18:56:31 +0530

    first

---
+++ FIRST/practical2.py
@@ -0,0 +1,36 @@
+#aim : bfs code
+from collections import defaultdict, deque
+
+class Graph:
+    def _init_(self):
+        self.graph = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def bfs(self, start):
+        visited = set()
+        queue = deque([start])
+        visited.add(start)
+
+        while queue:
+            node = queue.popleft()
+            print(node, end=" ")
+
+            for neighbor in self.graph[node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                    visited.add(neighbor)
+
+# Example usage:
+# Create a graph
+graph = Graph()
+graph.add_edge(0, 1)
+graph.add_edge(0, 2)
+graph.add_edge(1, 2)
+graph.add_edge(2, 0)
+graph.add_edge(2, 3)
+graph.add_edge(3, 3)
+
+print("BFS Traversal starting from vertex 2:")
+graph.bfs(2)
